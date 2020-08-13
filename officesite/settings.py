4dc2acb3fb6839595e7807096a466992f10e661b
Nikki Clark <nikclarks@protonmail.com>
2020-08-12 17:34:43 -0700

    dj-heroku settings

--- officesite/settings.py
+++ officesite/settings.py
@@ -24,7 +24,13 @@ class Base(Configuration):
     import os
     from django.core.exceptions import ImproperlyConfigured
 
+    import dj_database_url
+
+    DATABASES['default'] = dj_database_url.config(conn_max_age=600, ssl_require=True)
+
     BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+
+
 
     DATABASES = {
     'default': {
@@ -37,8 +43,6 @@ class Base(Configuration):
         }
     }
 
-    import dj_database_url
-    DATABASES['default'] = dj_database_url.config(conn_max_age=600, ssl_require=True)
 
     ALLOWED_HOSTS = []
 
@@ -128,6 +132,9 @@ class Prod(Base):
     django_heroku.settings(locals()) # activate django-heroku
 
     DEBUG = False
+
+
+    
 
 
 
