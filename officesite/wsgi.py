3d6983914af0991cbbb24738976bd0f8632cccb9
Nikki Clark <nikclarks@protonmail.com>
2020-08-12 17:25:14 -0700

    heroku wsgi

--- officesite/wsgi.py
+++ officesite/wsgi.py
@@ -12,6 +12,6 @@ import os
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'officesite.settings')
 os.environ.setdefault('DJANGO_CONFIGURATION', 'Dev')
 
-from django.core.wsgi import get_wsgi_application
+from configurations.wsgi import get_wsgi_application
 
 application = get_wsgi_application()
